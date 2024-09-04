8190759267bc380080aa281ec8c081b607d23515
Sumit <sumitthakur6500@gmail.com>
2024-09-04 23:40:07 +0530

    add

---
+++ javascript01/statements _expressions/sitaRam.js
@@ -0,0 +1,10 @@
+let marks = 76;
+if (marks >= 75) {
+    console.log("very Good");
+}
+else if (marks < 85 && marks >=50) {
+    console.log("Good");
+}
+else {
+    console.log("Needs Improvement");
+}
