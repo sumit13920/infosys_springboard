d92224c47bc93125de44a501d73d72da0f8fe5f5
Sumit <sumitthakur6500@gmail.com>
2024-09-06 09:51:24 +0530

    add

---
+++ javascript01/statements_expressions/ram.js
@@ -0,0 +1,18 @@
+ /*While writing client-Logic in JavaScript, variables and operators are often combined to do combined to do computations. This is achieved by writing expressions.*/
+
+ /*Different types of expressions that can be written in JavaScript are:*/
+
+ 10 + 30; //Evaluates to numeric value
+ "Hello" + "World"; //Evaluates to string value
+ itemRating > 5; //EValuates to boolean value
+ /*Evaluates to one string value based on whether condition is true or false.
+ if the condition evaluates to true then the first string value 
+ "senior citixen" is assigned otherwise the second string value is assigned "Normal citizen"*/
+
+//Example of an expression in a statement:
+
+var operator1 = 10;
+var operator2 = 50;
+var product = operator1 * operator2;
+
+console.log("Product");
